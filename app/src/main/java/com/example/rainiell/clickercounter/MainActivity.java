dc6facae9f8c127a165f27ce083229901574cb79
untouchableqtpies <hannahkylierain@gmail.com>
2016-11-19 17:19:57 +0800

    Android 2

---
+++ app/src/main/java/com/example/rainiell/clickercounter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.rainiell.clickercounter;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnClick;
+    Button btnReset;
+
+    TextView txtCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnClick = (Button)findViewById(R.id.buttonClick);
+        btnReset = (Button)findViewById(R.id.buttonReset);
+        txtCount = (TextView)findViewById(R.id.textViewClick);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String countValue= txtCount.getText().toString();
+                int intCountValue= Integer.parseInt(countValue);
+                intCountValue++;
+
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount.setText(String.valueOf(0));
+            }
+        });
+    }
+}
